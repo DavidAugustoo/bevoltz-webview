efb50f631eba7a7211a6777ea3b28cd09f9899df
David Augusto <davidribeiro086@gmail.com>
2023-07-30 14:34:29 -0300

    feat: add next secret key

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
     GOOGLE_CLIENT_ID:
       '982531789593-5o2e36pu6n7amhtpsblrrqahsl3jg4a8.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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',
+    NEXTAUTH_SECRET: 'LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx6gts=',
   },
 }
 
