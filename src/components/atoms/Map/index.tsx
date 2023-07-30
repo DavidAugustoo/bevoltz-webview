88363ec3ed4f8760053f97bf42a6c7f9c1ef886a
David Augusto <davidribeiro086@gmail.com>
2023-07-30 19:30:27 -0300

    feat: stations

--- src/components/atoms/Map/index.tsx
+++ src/components/atoms/Map/index.tsx
@@ -1,4 +1,14 @@
+import markerDark from '@assets/marker-dark.png'
 import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api'
+
+interface IStation {
+  title: string
+  cords: {
+    lat: number
+    lng: number
+  }
+  type: 'fast' | 'normal'
+}
 
 export function Map() {
   const { isLoaded } = useJsApiLoader({
@@ -6,9 +16,44 @@ export function Map() {
     googleMapsApiKey: 'AIzaSyDWPV4cI1tyKN6aznnGzzkP_6EJqNgGE6M',
   })
 
-  const position = {
-    lat: -23.57415,
-    lng: -46.62309,
+  const stations: IStation[] = [
+    {
+      title: 'Station 1',
+      cords: {
+        lat: -23.575328,
+        lng: -46.622491,
+      },
+      type: 'fast',
+    },
+    {
+      title: 'Station 2',
+      cords: {
+        lat: -23.57217,
+        lng: -46.624176,
+      },
+      type: 'fast',
+    },
+    {
+      title: 'Station 3',
+      cords: {
+        lat: -23.576979,
+        lng: -46.624114,
+      },
+      type: 'fast',
+    },
+    {
+      title: 'Station 4',
+      cords: {
+        lat: -23.571991,
+        lng: -46.621494,
+      },
+      type: 'fast',
+    },
+  ]
+
+  const stationTypes = {
+    fast: 'https://i.ibb.co/txykmBd/marker-dark.png',
+    normal: 'https://i.ibb.co/tY4Y30C/marker-light.png',
   }
 
   return (
@@ -16,7 +61,10 @@ export function Map() {
       {isLoaded ? (
         <GoogleMap
           mapContainerStyle={{ width: '100%', height: '100%' }}
-          center={position}
+          center={{
+            lat: -23.57415,
+            lng: -46.62309,
+          }}
           options={{
             streetViewControl: false, // desabilita o controle Street View
             scaleControl: false, // desabilita o controle de escala
@@ -27,7 +75,18 @@ export function Map() {
           }}
           zoom={17}
         >
-          <MarkerF position={position} />
+          <MarkerF
+            position={{
+              lat: -23.57415,
+              lng: -46.62309,
+            }}
+          />
+
+          {stations.map(({ cords, title, type }) => {
+            return (
+              <MarkerF key={title} position={cords} icon={stationTypes[type]} />
+            )
+          })}
         </GoogleMap>
       ) : (
         <>
