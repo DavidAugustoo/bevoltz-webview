ad89e1c959ef687b3c37b97f52b02b9e56fb352a
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2023-08-10 08:38:04 -0300

    Add files via upload

--- src/components/atoms/DateInput/index.tsx
+++ src/components/atoms/DateInput/index.tsx
@@ -1,14 +1,32 @@
-import { useState, forwardRef } from 'react'
+import React, { forwardRef } from 'react'
+import { useState } from 'react'
 import DatePicker from 'react-datepicker'
+
 import 'react-datepicker/dist/react-datepicker.css'
+import { CustomDataPickerButton, DataPickerContainer } from './styles'
+
+const CustomInput = forwardRef(({ value, onClick }: any, ref: any) => (
+  <CustomDataPickerButton
+    className="custom-date-input"
+    onClick={onClick}
+    ref={ref}
+  >
+    {value}
+  </CustomDataPickerButton>
+))
+
+CustomInput.displayName = 'CustomInput'
 
 export function DateInput() {
   const [startDate, setStartDate] = useState(new Date())
 
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date as Date)}
-    />
+    <DataPickerContainer>
+      <DatePicker
+        customInput={<CustomInput />}
+        selected={startDate}
+        onChange={(date) => setStartDate(date as Date)}
+      />
+    </DataPickerContainer>
   )
 }
--- src/components/atoms/DateInput/styles.ts
+++ src/components/atoms/DateInput/styles.ts
@@ -2,6 +2,28 @@ import DatePicker from 'react-datepicker'
 
 import styled from 'styled-components'
 
-export const DataPickerContainer = styled(DatePicker)`
-  background-color: red;
+export const DataPickerContainer = styled('div')`
+  .react-datepicker-wrapper {
+    width: 100%;
+  }
+`
+
+export const DataPickerElement = styled(DatePicker)`
+  background-color: #f5f5f5;
+  padding: 14px 10px;
+  border: 0px;
+  border-radius: 5px;
+  width: 100%;
+
+  input {
+    width: 100%;
+  }
+`
+
+export const CustomDataPickerButton = styled('div')`
+  background-color: #f5f5f5;
+  padding: 14px 10px;
+  border: 0px;
+  border-radius: 5px;
+  width: 100%;
 `
