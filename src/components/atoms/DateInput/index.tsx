8c8e61b566977bc86743ecd73ef6d13db86378d9
David Augusto <davidribeiro086@gmail.com>
2023-08-18 16:48:56 -0300

    feat: DateInput component

--- src/components/atoms/DateInput/index.tsx
+++ src/components/atoms/DateInput/index.tsx
@@ -3,17 +3,39 @@ import { useState } from 'react'
 import DatePicker from 'react-datepicker'
 
 import 'react-datepicker/dist/react-datepicker.css'
+import { Text } from '../Text'
 import { CustomDataPickerButton, DataPickerContainer } from './styles'
 
-const CustomInput = forwardRef(({ value, onClick }: any, ref: any) => (
-  <CustomDataPickerButton
-    className="custom-date-input"
-    onClick={onClick}
-    ref={ref}
-  >
-    {value}
-  </CustomDataPickerButton>
-))
+import { CalendarBlank } from '@phosphor-icons/react'
+import { format, parse } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+const CustomInput = forwardRef(({ value, onClick }: any, ref: any) => {
+  const formatDateAsString = (dateString: string) => {
+    console.log(dateString)
+
+    const date = parse(dateString, 'MM/dd/yyyy', new Date())
+    return format(date, "dd 'de' MMMM, yyyy", { locale: ptBR })
+  }
+
+  const formattedDate = formatDateAsString(value)
+
+  return (
+    <CustomDataPickerButton
+      className="custom-date-input"
+      onClick={onClick}
+      ref={ref}
+    >
+      <Text
+        content={formattedDate}
+        color="gray_100"
+        size="xxsmall"
+        weight="light"
+      />
+      <CalendarBlank size={22} color="#515151" />
+    </CustomDataPickerButton>
+  )
+})
 
 CustomInput.displayName = 'CustomInput'
 
@@ -26,6 +48,7 @@ export function DateInput() {
         customInput={<CustomInput />}
         selected={startDate}
         onChange={(date) => setStartDate(date as Date)}
+        popperPlacement="auto"
       />
     </DataPickerContainer>
   )
--- src/components/atoms/DateInput/styles.ts
+++ src/components/atoms/DateInput/styles.ts
@@ -6,6 +6,33 @@ export const DataPickerContainer = styled('div')`
   .react-datepicker-wrapper {
     width: 100%;
   }
+
+  .react-datepicker {
+    width: 67vw;
+    transform: scale(1.5);
+    transform-origin: top left;
+  }
+
+  .react-datepicker__month-container {
+    width: 100% !important;
+  }
+
+  .react-datepicker-popper {
+    position: fixed !important; /* Usamos 'fixed' para posicionar o datepicker em relação à janela do navegador */
+    top: 37% !important; /* Centraliza verticalmente */
+    left: 33% !important; /* Centraliza horizontalmente */
+    transform: translate(
+      -50%,
+      -50%
+    ) !important; /* Ajusta para que o centro do datepicker esteja exatamente no centro da tela */
+    z-index: 1050 !important; /* Garante que o datepicker apareça acima de outros elementos; ajuste conforme necessário */
+  }
+
+  .react-datepicker-popper[data-placement^='bottom']
+    .react-datepicker__triangle,
+  .react-datepicker-popper[data-placement^='top'] .react-datepicker__triangle {
+    display: none !important;
+  }
 `
 
 export const DataPickerElement = styled(DatePicker)`
@@ -26,4 +53,7 @@ export const CustomDataPickerButton = styled('div')`
   border: 0px;
   border-radius: 5px;
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
