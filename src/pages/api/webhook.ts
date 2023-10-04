02233f4e81a096f493b423e58e8fbe3a5af1617b
David Augusto <davidribeiro086@gmail.com>
2023-10-04 14:09:39 -0300

    chore: error page

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -4,19 +4,8 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    const paymentData = req.body
-
-    if (
-      paymentData.status === 'approved' &&
-      paymentData.payment_type_id === 'pix'
-    ) {
-      res.writeHead(302, { Location: '/success' })
-      return res.end()
-    } else {
-      res.status(200).send({
-        message: 'Notificação recebida, mas não é um pagamento PIX aprovado.',
-      })
-    }
+    res.writeHead(302, { Location: '/success' })
+    return res.end()
   } else {
     res.status(405).send({ message: 'Método não permitido.' })
   }
